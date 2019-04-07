86dc2601577e16052e759cbea784e32f2131f49b
xingfeng2010 <583354000@qq.com>
2019-04-07 17:49:49 +0800

    init

---
+++ ffmpeg-3.3.9/build_android.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+make clean
+# NDK的路径，根据自己的安装位置进行设置
+export TMPDIR=/Users/lishixing/Desktop/document/learn/multimedialearn/ffmpeg-3.3.9/ffmpeg_install
+export NDK=/Users/lishixing/Software/android-ndk-r14b
+export SYSROOT=$NDK/platforms/android-21/arch-arm/
+export TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+export CPU=arm
+export PREFIX=$(pwd)/android/$CPU
+export ADDI_CFLAGS="-marm"
+function build_one
+{
+./configure \
+    --prefix=$PREFIX \
+    --target-os=linux \
+    --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+    --arch=arm \
+    --sysroot=$SYSROOT \
+    --extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    --cc=$TOOLCHAIN/bin/arm-linux-androideabi-gcc \
+    --nm=$TOOLCHAIN/bin/arm-linux-androideabi-nm \
+    --target-os=android \
+    --enable-shared \
+    --enable-runtime-cpudetect \
+    --enable-gpl \
+    --enable-small \
+    --enable-cross-compile \
+    --disable-debug \
+    --disable-static \
+    --disable-doc \
+    --disable-asm \
+    --enable-ffmpeg \
+    --enable-ffplay \
+    --enable-ffprobe \
+    --disable-ffserver \
+    --enable-postproc \
+    --enable-avdevice \
+    --disable-symver \
+    --disable-stripping \
+$ADDITIONAL_CONFIGURE_FLAG
+sed -i '' 's/HAVE_LRINT 0/HAVE_LRINT 1/g' config.h
+sed -i '' 's/HAVE_LRINTF 0/HAVE_LRINTF 1/g' config.h
+sed -i '' 's/HAVE_ROUND 0/HAVE_ROUND 1/g' config.h
+sed -i '' 's/HAVE_ROUNDF 0/HAVE_ROUNDF 1/g' config.h
+sed -i '' 's/HAVE_TRUNC 0/HAVE_TRUNC 1/g' config.h
+sed -i '' 's/HAVE_TRUNCF 0/HAVE_TRUNCF 1/g' config.h
+sed -i '' 's/HAVE_CBRT 0/HAVE_CBRT 1/g' config.h
+sed -i '' 's/HAVE_RINT 0/HAVE_RINT 1/g' config.h
+make clean
+# 这里是定义用几个CPU编译，我用4个，一般在5分钟之内编译完成
+make -j4
+make install
+}
+build_one
